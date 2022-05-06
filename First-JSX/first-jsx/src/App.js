7f450bcba45c117f3127bcde4946c3897be2190b
Capital-Estama <capitalestama@gmail.com>
2022-05-05 23:27:01 -0400

    First Jsx commit 1

---
+++ First-JSX/first-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li><h3>Learn react</h3></li>
+        <li><h3>Climb Mt. Everest</h3></li>
+        <li><h3>Run a marathon</h3></li>
+        <li><h3>Feed the dogs</h3></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
