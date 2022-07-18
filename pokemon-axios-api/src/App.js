3d1213613411a98654b346a616eb1fbf8ecb990c
Wes <air@Wess-Air.tampabay.rr.com>
2022-07-18 04:48:34 -0400

    Fixed

---
+++ pokemon-axios-api/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Pokemon from './componets/pokemon';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokemon/>
+    </div>
+  );
+}
+
+export default App;
